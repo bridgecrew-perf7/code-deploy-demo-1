5df35617271ea38a2057f46b623d43fa6003d5f7
minwoo <mwoodev526@gmail.com>
2021-09-23 03:09:13 +0900

    [update] BITBUCKET_BUILD_NUMBER

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const port = process.env.port | 3001
 
 app.get("/", function(req, res){
-  res.status(200).send("cd-ec2-1 IAM Bug Fix and Running! - testServer01");
+  res.status(200).send("cd-ec2-1 IAM Bug Fix and Running! - testServer01 - BITBUCKET_BUILD_NUMBER");
 });
 
 app.listen(port,()=>{
   console.log(`Node server started on port : ${port}`)
 });
+q
